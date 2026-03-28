042cab5eb2e98642d4880f6290d0216c9e7bb45e
Daniel Nolen <dnolen@smu.edu>
2026-03-28 16:07:14 +0000

    Add FAAB source model

---
+++ models/source/ftn/src_ftn_faab.sql
@@ -0,0 +1,21 @@
+{{
+    config(
+        materialized='table'
+    )
+}}
+
+select {{ dbt_utils.star(source('ftn', 'faab')) }},
+    regexp_extract("$path", 'year=([0-9]{4})', 1) as year,
+    regexp_extract("$path", 'month=([0-9]{1,2})', 1) as month,
+    regexp_extract("$path", 'day=([0-9]{1,2})', 1) as day,
+    concat(regexp_extract("$path", 'year=([0-9]{4})', 1),
+        regexp_extract("$path", 'month=([0-9]{1,2})', 1),
+        regexp_extract("$path", 'day=([0-9]{1,2})', 1)) as _ptkey,
+    element_at(SPLIT("$path", '/'), -1) as _filename,
+    current_timestamp as _loaddatetime
+from {{ source('ftn', 'faab') }}
+where concat(regexp_extract("$path", 'year=([0-9]{4})', 1),
+    regexp_extract("$path", 'month=([0-9]{1,2})', 1),
+    regexp_extract("$path", 'day=([0-9]{1,2})', 1)) = (select max(concat(regexp_extract("$path", 'year=([0-9]{4})', 1),
+                                                        regexp_extract("$path", 'month=([0-9]{1,2})', 1),
+                                                        regexp_extract("$path", 'day=([0-9]{1,2})', 1))) from {{ source('ftn', 'faab') }})
